66ff828afe3e378d27f68b1a743b2ac2576deb84
Bruno <bruvanjura@gmail.com>
2026-06-14 11:05:56 -0300

    mesclando códigos

--- funcao.py
+++ funcao.py
@@ -1,6 +1,7 @@
 import math
 
 def calculate_velocity(target, current):
+
     """
     
     Retornar uma lista com velx e w
@@ -18,6 +19,9 @@ def calculate_velocity(target, current):
     vel = [0.0, 0.0]
     pos_rel = [target[0] - current[0], target[1] - current[1]]
 
+    vel = [0.0, 0.0]
+
+
 
     # math.atan2 lida com divisão por zero e corrige o quadrante do ângulo
     angle_real = math.atan2(pos_rel[1], pos_rel[0])
@@ -31,4 +35,27 @@ def calculate_velocity(target, current):
         vel[1] = erro*0.1
         return vel
     
+
+
+
+    #State 2
+
+    robo_apontando_destino = True
+
+
+    distancia_local = (((target[0] - current[0]))**2 + (target[1] - current[1])**2)**(1/2)
+
+    erro_x = (target[0] - current[0])/10
+    erro_y = (target[1] - current[1])/10
+    
+    if robo_apontando_destino and current:
+        vel[0] = 0.2
+     
+
+     
+
+
+
+
+
     return vel
